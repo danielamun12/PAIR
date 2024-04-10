62c0edfff60265a89c5114489a3d41e8d0af1e36
danielamun12 <nanismunoz@comcast.net>
2024-04-10 11:13:25 -0400

    Initial Commit

---
+++ PAIR/PAIRApp.swift
@@ -0,0 +1,17 @@
+//
+//  PAIRApp.swift
+//  PAIR
+//
+//  Created by Daniela Munoz on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PAIRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
